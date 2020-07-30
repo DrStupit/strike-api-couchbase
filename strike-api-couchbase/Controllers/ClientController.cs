b226ba21d0e10b5d51f3768e69d345c6ccc1946f
Kerev Reddy <KerevR@betsolutions.net>
2020-07-30 06:52:22 +0200

    Clean unused files and add validation to balance calls

--- strike-api-couchbase/Controllers/ClientController.cs
+++ strike-api-couchbase/Controllers/ClientController.cs
@@ -6,6 +6,7 @@ using Couchbase.Core;
 using Couchbase.Extensions.DependencyInjection;
 using Couchbase.N1QL;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using strike_api_couchbase.Models.Balance;
 
@@ -15,24 +16,34 @@ namespace strike_api_couchbase.Controllers
     [ApiController]
     public class ClientController : ControllerBase
     {
+        private readonly ILogger<ClientController> _logger;
         private readonly IBucket _bucket;
-        public ClientController(IBucketProvider bucketProvider)
+        public ClientController(IBucketProvider bucketProvider, ILogger<ClientController> logger)
         {
             _bucket = bucketProvider.GetBucket("balance");
+            _logger = logger;
         }
+
         [HttpGet] 
         [Route("getBalance")]
         public List<Balance> GetBalance(long punterId)
         {
             var records = new List<Balance>();
             var n1ql = "SELECT d.* FROM balance d WHERE punterID = $punterID";
-            var qr = QueryRequest.Create(n1ql);
-            qr.AddNamedParameter("$punterID", punterId);
-            var queryResult = _bucket.Query<Balance>(qr);
-            foreach(var row in queryResult.Rows)
+            var queryRequest = QueryRequest.Create(n1ql);
+            queryRequest.AddNamedParameter("$punterID", punterId);
+            var queryResult = _bucket.Query<Balance>(queryRequest);
+
+            if(!queryResult.Success)
             {
-                
-                records.Add(row);
+                _logger.LogInformation($"Failed to get balance for {punterId}");
+            } else
+            {
+                foreach (var row in queryResult.Rows)
+                {
+
+                    records.Add(row);
+                }
             }
             return records;
         }
@@ -41,7 +52,10 @@ namespace strike_api_couchbase.Controllers
         [Route("updateBalance")]
         public string UpdateBalance(Balance balance)
         {
-            balance.ID = Guid.NewGuid().ToString();
+            if(string.IsNullOrEmpty(balance.ID))
+            {
+                balance.ID = Guid.NewGuid().ToString();
+            }
             var result = _bucket.Upsert<Balance>(balance.ID, balance);
 
             if(result.Success)
