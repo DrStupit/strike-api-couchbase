cbb429ee1aeab3597113e6cb4d2273f051fae573
Kerev Reddy <KerevR@betsolutions.net>
2020-07-30 14:47:34 +0200

    Map Odds Listings

---
+++ strike-api-couchbase/Controllers/SportController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Couchbase.Core;
+using Couchbase.Extensions.DependencyInjection;
+using Couchbase.N1QL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Strike.Common.Models.Feeds;
+using strike_api_couchbase.Models.Balance;
+
+namespace strike_api_couchbase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SportController : ControllerBase
+    {
+        private readonly ILogger<SportController> _logger;
+        private readonly IBucket _bucket;
+        public SportController(IBucketProvider bucketProvider, ILogger<ClientController> logger)
+        {
+            _bucket = bucketProvider.GetBucket("feeds");
+        }
+
+        #region Sports
+        [HttpGet]
+        [Route("getSports")]
+        public List<Sport> GetSports()
+        {
+            var records = new List<Sport>();
+            var n1ql = "SELECT s.* FROM feeds s WHERE active = true";
+            var queryRequest = QueryRequest.Create(n1ql);
+            var queryResult = _bucket.Query<Sport>(queryRequest);
+
+            if (!queryResult.Success)
+            {
+                _logger.LogInformation($"Failed to get balance for {queryRequest}");
+            }
+            else
+            {
+                foreach (var row in queryResult.Rows)
+                {
+                    records.Add(row);
+                }
+            }
+            return records;
+        }
+        #endregion
+    }
+}
---
+++ strike-api-couchbase/Models/Sports/OddsResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace strike_api_couchbase.Models.Sports
+{
+    public class OddsResponse
+    {
+        public bool Success { get; set; }
+        public List<ListingOptions> Data { get; set; }
+        public int Sites_Count { get; set; }
+    }
+
+    public class ListingOptions
+    {
+        public string Sport_Key { get; set; }
+        public string Sport_Nice { get; set; }
+        public List<string> Teams { get; set; }
+        public DateTime Commence_Time { get; set; }
+        public string Home_Team { get; set; }
+        public List<Sites> Sites { get; set; }
+        public string Type { get { return "offered-odds"; } }
+    }
+
+    public class Sites
+    {
+        public string Site_Key { get; set; }
+        public string Site_Nice { get; set; }
+        public DateTime Last_Update { get; set; }
+        public Odds[] Odds { get; set; }
+    }
+
+    public class Odds
+    {
+        public List<string> H2H { get; set; }
+    }
+}
